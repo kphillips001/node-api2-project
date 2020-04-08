65eb9d4cfc0ec680bfd3779b86479932a360d0e7
Kevin <knphillips01@gmail.com>
2020-04-07 21:33:39 -0500

    finished //GET request to /api/posts

--- posts/router.js
+++ posts/router.js
@@ -50,4 +50,20 @@ router.post('/:id/comments', (req, res) => {
   }
 });
 
+//GET request to /api/posts
+router.get('/', (req, res) => {
+  Posts.find(req.query)
+  .then(posts => {
+    res.status(200).json(posts);
+  })
+  .catch(err => {
+    console.log('Error', error);
+    res.status(500).json({
+      error: "The posts information could not be retrieved."
+    });
+  });
+})
+
+//GET request to /api/posts/:id
+
 module.exports = router; 
