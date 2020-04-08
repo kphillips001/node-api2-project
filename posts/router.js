a293dbfeb160e16f428e5d1e008e28a5148df876
Kevin <knphillips01@gmail.com>
2020-04-07 21:51:43 -0500

    mvp

--- posts/router.js
+++ posts/router.js
@@ -84,4 +84,68 @@ router.get('/:id', (req, res) => {
   })
 })
 
+//GET request to /api/posts/:id/comments
+router.get('/:id/comments', (req, res) => {
+  const commentId = req.params.id;
+  Posts.findPostComments(commentId)
+  .then(posts => {
+    if (posts.length === 0) {
+      resp.status(404).json({
+         message: 'The post with the specified ID does not exist.'
+       });
+    } else {
+     res.status(200).json(posts);
+    }
+      })
+      .catch(error => {
+          console.log("Error: ", error);
+          res.status(500).json({ error: 'The comments information could not be retrieved.' });
+      });
+});
+// DELETE request to /api/posts/:id
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  Posts.remove(id)
+      .then(posts => {
+          if (!posts) {
+              res.status(404).json({
+                  message: "The post with the specified ID does not exist."
+              });
+          } else {
+              res.status(200).json(posts);
+          }
+      })
+      .catch(error => {
+          console.log("Error: ", error);
+          res
+              .status(500)
+              .json({ errorMessage: "The post could not be removed" });
+      });
+});
+// PUT request to /api/posts/:id
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+  Posts.update(id, updates)
+      .then(posts => {
+          if (!posts) {
+              res
+                  .status(404)
+                  .json({ message: "The post with the specified ID does not exist." });
+          } else if (!updates.title || !updates.contents) {
+              res.status(400).json({
+                  errorMessage: "Please provide title and contents for the post."
+              });
+          } else {
+              res.status(200).json(posts);
+          }
+      })
+      .catch(error => {
+          console.log("Error: ", error);
+          res
+              .status(500)
+              .json({ error: "The post information could not be modified." });
+      });
+});
+
 module.exports = router; 
