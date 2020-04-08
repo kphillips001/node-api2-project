b7df42e574579af36896771a0a998875492ba8f5
Kevin <knphillips01@gmail.com>
2020-04-07 20:58:23 -0500

    completed Put request to /api/posts

--- posts/router.js
+++ posts/router.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const router = express.Router();
+const Posts = require('../data/db')
 
-const db = require('../data/db')
-
-const router = express.Router()
+//Put request to /api/posts
+router.post('/', {req, res} => {
+  const postItems = req.body;
+  if (!postItems.title || !postItems.contents) {
+    res.status(400).json({
+      errorMessage: "Please provide title and contents for the post."
+    })
+  } else {
+    Posts.insert(postItems)
+    .then(posts => {
+      res.status(201).json(posts)
+    })
+    .catch(err => {
+      console.log('Error', err);
+      res.status(500).json({
+        error: "There was an error while saving the post to the database"
+      })
+    })
+  }
+})
 
 module.exports = router; 
