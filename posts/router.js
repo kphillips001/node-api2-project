1f0fa6cb2b7ede994615594b68c3e5c6a28c3aa9
Kevin <knphillips01@gmail.com>
2020-04-07 21:46:19 -0500

    finished get requests to posts

--- posts/router.js
+++ posts/router.js
@@ -65,5 +65,23 @@ router.get('/', (req, res) => {
 })
 
 //GET request to /api/posts/:id
+router.get('/:id', (req, res) => {
+  Posts.findById(req.params.id)
+  .then(posts => {
+    if (posts) {
+      res.status(200).json(posts)
+    } else {
+      res.status(404).json({
+        message: "The post with the specified ID does not exist."
+      })
+    }
+  })
+  .catch(error => {
+    console.log('Error', error);
+    res.status(500).json({
+      error: "The post information could not be retrieved."
+    })
+  })
+})
 
 module.exports = router; 
